b8f2ae97053e47f84b6321e4d936683c59052750
Szymon Przedwojski <szymon.przedwojski@bms.com.pl>
2016-12-20 12:39:25 +0100

    Ikona w trayu aktualizuje się co 5 sekund.

--- tray.py
+++ tray.py
@@ -1,47 +1,45 @@
-from gi.repository import Gtk as gtk
+import os
+import requests
+import signal
+import sys
 from gi.repository import AppIndicator3 as appindicator
+from gi.repository import GLib as glib
+from gi.repository import Gtk as gtk
 from requests.auth import HTTPBasicAuth
-import signal, os, json, requests, sys
 
 APPINDICATOR_ID = 'myappindicator'
 
-def main():
-    if(len(sys.argv) < 2):
-        print "Room id not set"
-        sys.exit()
-    room_id = sys.argv[1]
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, 'no icon', appindicator.IndicatorCategory.SYSTEM_SERVICES)
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(gtk.Menu())
-#    indicator.set_icon(os.path.abspath('circle_red.png'))
-#    indicator = appindicator.Indicator.new(APPINDICATOR_ID, gtk.STOCK_YES, appindicator.IndicatorCategory.SYSTEM_SERVICES)
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-#    gtk.main()
+
+def update(room_id, indicator):
     url = "https://hackathon-heroku-app.herokuapp.com/occupied"
-#    response = urllib.urlopen(url)
     response = requests.get(url, auth=HTTPBasicAuth('1', '123456'))
-#    data = json.loads(response)
-#    print data
     json_arr = response.json()
     occupied = None
     for room in json_arr:
-#        print room
-        if(room['id'] == int(room_id)):
+        if room['id'] == int(room_id):
             print room
             occupied = room['occupied']
-    print occupied
-    img_path = ""
-    if(occupied):
-        print "inside occupied"
+    if occupied:
         img_path = os.path.abspath('circle_red.png')
-    elif(occupied == False):
-        print "inside not occupied"
+    elif not occupied:
         img_path = os.path.abspath('circle_green.png')
     else:
-        print "inside unknown"
         img_path = os.path.abspath('unknown.png')
-    print img_path
     indicator.set_icon(img_path)
+    return True
+
+
+def main():
+    if(len(sys.argv) < 2):
+        print "Room id not set. Pass room id as the first argument of the script, e.g. 'python tray.py 1'"
+        sys.exit()
+    room_id = sys.argv[1]
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('unknown.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(gtk.Menu())
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    update(room_id, indicator)
+    glib.timeout_add_seconds(5, update, room_id, indicator)
     gtk.main()
 
 if __name__ == "__main__":
